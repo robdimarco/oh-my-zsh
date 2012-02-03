9fc59b165ddfc9954953db9bc1f00f51c0adbec7
Rob Di Marco <rob@innovationontherun.com>
2012-02-03 08:34:01 -0500

    Add a ruby exec

--- custom/plugins/rdimarco/rdimarco.plugin.zsh
+++ custom/plugins/rdimarco/rdimarco.plugin.zsh
@@ -21,4 +21,12 @@ _rbtest_with_rake() {
     bundle exec rake test
   fi
 }
+_rbtest_without_rake() {
+  if [ -f $1 ]; then
+    bundle exec ruby -Itest $@;
+  else
+    echo "Please specify a file(s) to test"
+  fi;
+}
 eval "function rbtest () { _rbtest_with_rake \$@}"
+eval "function rbtest_exec(){_rbtest_without_rake \$@}"
